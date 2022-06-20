a96680d09909e38de23043f72b52a97e55607962
eriktroendle <erik.troendle@campus.tu-berlin.de>
2022-06-20 19:02:19 +0000

    Resolve "proper control over window layout"

--- Python Dateien/Übung 03.py
+++ Python Dateien/Übung 03.py
@@ -17,7 +17,6 @@ plt.use('TkAgg')
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 
-#test_data_x = np.linspace(0, 100, 100)
 test_data_y = np.linspace(0, 100, 100)
 
 window = tk.Tk()
@@ -50,31 +49,15 @@ verstärkung_scale = tk.Scale(window, showvalue=1, orient='horizontal', variable
 linearität_scale = tk.Scale(window, showvalue=1, orient='horizontal', variable=linearität_value, from_=-100.00, to=100.00)
 
 
-figure_canvas.get_tk_widget().grid(row = 0,column = 1, columnspan = 3)
-offset_checkbox.grid(row = 1,column = 0)
-verstärkung_checkbox.grid(row = 2,column = 0)
-linearität_checkbox.grid(row = 3,column = 0)
-offset_scale.grid(row = 1,column = 1, columnspan = 2)
-verstärkung_scale.grid(row = 2,column = 1, columnspan = 2)
-linearität_scale.grid(row = 3,column = 1, columnspan = 2)
-
-
-# def button_action():
-#     anweisungs_label.config(text="Ich wurde geöndert")
-
-
-
-
-# change_btn = tk.Button(window, text = "Ändern", command = button_action)
-# exit_btn = tk.Button(window, text = "Exit", command = window.quit)
-
-# anweisungs_label = tk.Label(window, text = "Ich bin eine Anweisung")
-# info_label = tk.Label(window, text = "Ich bin ein Info Label:\nDer Beenden Button schließt das Fenster!")
-
-# anweisungs_label.pack()
-# change_btn.pack()
-# info_label.pack()
-# exit_btn.pack()
+window.columnconfigure(0, weight=0)
+window.columnconfigure(1, weight=10)
+figure_canvas.get_tk_widget().grid(row = 0, column = 0, columnspan = 2)
+offset_checkbox.grid(row = 1, column = 0, sticky = 'w', pady = 10, padx = 40)
+verstärkung_checkbox.grid(row = 2, column = 0, sticky = 'w', pady = 10, padx = 40)
+linearität_checkbox.grid(row = 3, column = 0, sticky = 'w', pady = 10, padx = 40)
+offset_scale.grid(row = 1, column = 1, sticky = 'ew', pady = 10, padx = 40)
+verstärkung_scale.grid(row = 2, column = 1, sticky = 'ew', pady = 10, padx = 40)
+linearität_scale.grid(row = 3, column = 1, sticky = 'ew', pady = 10, padx = 40)
 
 if __name__ == "__main__":
     window.mainloop()
