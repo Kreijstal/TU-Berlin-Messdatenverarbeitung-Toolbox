2a74ee99b03b78b8154c2b164a4d9cc83d8f162b
eriktroendle <erik.troendle@campus.tu-berlin.de>
2022-06-28 14:14:54 +0000

    Resolve "Create single errorcurves"

--- Python Dateien/Übung 03.py
+++ Python Dateien/Übung 03.py
@@ -14,21 +14,34 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 window_title = "Übung 03 - Statische Eigenschaften von Messystemen"
 
 class TKWindow(tk.Tk):
-    axes_params = {"bounds": 100,
-                   "title": "Kennlinien Fehler",
-                   "xlabel": "Output",
-                   "ylabel": "Input",
-                   "plot1": {"label": "reale Kennlinie",
-                             "color": "blue",
-                             "linestyle": "-"},
-                   "plot2": {"label": "ideale Kennlinie",
-                             "color": "red",
-                             "linestyle": "--"},
-                   "legend_position": "upper left"}
+    figure_params = {"figsize": (8,5),
+                     "title": "Kennlinien Fehler",
+                     "xlabel": "Output",
+                     "ylabel": "Input",
+                     "bounds": 100,
+                     "legend_position": "upper left"}
     
-    graphs = {"x": np.linspace(0, axes_params["bounds"], axes_params["bounds"]+1), 
-             "y": np.zeros(axes_params["bounds"]+1),
-             "y_ideal": np.linspace(0, axes_params["bounds"], axes_params["bounds"]+1)}
+    graphs = {"x": np.linspace(0, figure_params["bounds"], figure_params["bounds"]+1),
+              "y": {"curve": np.zeros(figure_params["bounds"]+1), 
+                    "label": "reale Kennlinie", 
+                    "color": "blue",
+                    "linestyle": "solid"},
+              "y_ideal": {"curve": np.linspace(0, figure_params["bounds"], figure_params["bounds"]+1), 
+                          "label": "ideale Kennlinie", 
+                          "color": "red",
+                          "linestyle": "dashed"},
+              "y_offset_error": {"curve": np.linspace(0, figure_params["bounds"], figure_params["bounds"]+1), 
+                                 "label": "Kennlinie mit Offset-Fehler", 
+                                 "color": "green",
+                                 "linestyle": "dashdot"},
+              "y_amplification_error": {"curve": np.linspace(0, figure_params["bounds"], figure_params["bounds"]+1), 
+                                        "label": "Kennlinie mit Verstärkungs-Fehler", 
+                                        "color": "yellowgreen",
+                                        "linestyle": "dashdot"},
+              "y_linearity_error": {"curve": np.linspace(0, figure_params["bounds"], figure_params["bounds"]+1),
+                                    "label": "Kennlinie mit Linearitäts-Fehler", 
+                                    "color": "darkgreen",
+                                    "linestyle": "dashdot"}}
     
     widget_params = {"offset_checkbox": {"name": "offset",
                                           "text": "Offsetfehler",
@@ -77,7 +90,7 @@ class TKWindow(tk.Tk):
         self.widget_params["linearity_scale"]["variable"] = tk.DoubleVar()
     
     def init_figure(self):
-        self.figure = Figure(figsize=(6, 4), dpi=100)
+        self.figure = Figure(figsize=self.figure_params["figsize"], dpi=100)
         self.figure_canvas = FigureCanvasTkAgg(self.figure, self)
     
     def init_axes(self):
@@ -143,37 +156,42 @@ class TKWindow(tk.Tk):
         self.mainloop()
     
     def animate(self, i):
-        self.update_graph()
+        self.update_graphs()
         self.axes.clear()
-        self.axes.set_title(self.axes_params["title"])
-        self.axes.set_xlabel(self.axes_params["xlabel"])
-        self.axes.set_ylabel(self.axes_params["ylabel"])
+        self.axes.set_title(self.figure_params["title"])
+        self.axes.set_xlabel(self.figure_params["xlabel"])
+        self.axes.set_ylabel(self.figure_params["ylabel"])
         self.axes.autoscale(False)
         self.axes.grid()
         self.axes.minorticks_on()
-        self.axes.set_xbound(lower = 0, upper = self.axes_params["bounds"])
-        self.axes.set_ybound(lower = 0, upper = self.axes_params["bounds"])
-        self.axes.plot(self.graphs["x"], 
-                       self.graphs["y"], 
-                       label = self.axes_params["plot1"]["label"], 
-                       color = self.axes_params["plot1"]["color"], 
-                       linestyle = self.axes_params["plot1"]["linestyle"])
-        self.axes.plot(self.graphs["x"], 
-                       self.graphs["y_ideal"], 
-                       label = self.axes_params["plot2"]["label"], 
-                       color = self.axes_params["plot2"]["color"], 
-                       linestyle = self.axes_params["plot2"]["linestyle"])
-        self.axes.legend(loc=self.axes_params["legend_position"])
+        self.axes.set_xbound(lower = 0, upper = self.figure_params["bounds"])
+        self.axes.set_ybound(lower = 0, upper = self.figure_params["bounds"])
+        self.plot_axes(self.graphs["y"])
+        self.plot_axes(self.graphs["y_ideal"])
+        self.plot_axes(self.graphs["y_offset_error"]) if self.widget_params['offset_checkbox']["variable"].get() else None
+        self.plot_axes(self.graphs["y_amplification_error"]) if self.widget_params['amplification_checkbox']["variable"].get() else None
+        self.plot_axes(self.graphs["y_linearity_error"]) if self.widget_params['linearity_checkbox']["variable"].get() else None
+        self.axes.legend(loc=self.figure_params["legend_position"])
     
-    def update_graph(self):
+    def update_graphs(self):
         a = self.widget_params["offset_scale"]["variable"].get() if self.widget_params['offset_checkbox']["variable"].get() else 0
         b = 1 + self.widget_params["amplification_scale"]["variable"].get()/(100 if self.widget_params["amplification_scale"]["variable"].get() < 0 else 10) \
         if self.widget_params['amplification_checkbox']["variable"].get() else 1
         c = self.widget_params["linearity_scale"]["variable"].get() if self.widget_params['linearity_checkbox']["variable"].get() else 0
-        d = self.axes_params["bounds"]/2
+        d = self.figure_params["bounds"]/2
         
-        for x in range(0, self.axes_params["bounds"]+1):
-            self.graphs["y"][x] = a + b*x - c*((x/d - 1)**2 - 1)
+        for x in range(0, self.figure_params["bounds"]+1):
+            self.graphs["y"]["curve"][x] = a + b*x - c*((x/d - 1)**2 - 1)
+            self.graphs["y_offset_error"]["curve"][x] = a + x
+            self.graphs["y_amplification_error"]["curve"][x] = b*x
+            self.graphs["y_linearity_error"]["curve"][x] = x - c*((x/d - 1)**2 - 1)
+    
+    def plot_axes(self, plot):
+        self.axes.plot(self.graphs["x"], 
+                       plot["curve"], 
+                       label = plot["label"], 
+                       color = plot["color"], 
+                       linestyle = plot["linestyle"])
 
 if __name__ == "__main__":
     exercise_03 = TKWindow(window_title)
