af9a9d26b8c2a34952f1fc2d211bf3be55e39465
Erik Tröndle <erik.troendle@gmx.net>
2022-10-12 20:00:05 +0200

    modified:   Python_Dateien/Ubung_05.py

--- Python_Dateien/Ubung_05.py
+++ Python_Dateien/Ubung_05.py
@@ -21,38 +21,30 @@ class TKWindow(tk.Tk):
                      "title 1": "ADU Kennlinie",
                      "xlabel 1": "Input",
                      "ylabel 1": "Codes",
-                     "title 2": "Kennlinien Delta",
+                     "title 2": "Quantisierungsrauschen",
                      "xlabel 2": "Input",
-                     "ylabel 2": "\delta Codes",
-                     "bounds": 100,
+                     "ylabel 2": "\delta Kennlinien",
+                     "bounds": 1000,
                      "legend_position": "upper left"}
-    # ToDo
+    
     graphs = {"x": np.linspace(0, figure_params["bounds"], figure_params["bounds"]+1),
-              "y": {"curve": np.zeros(figure_params["bounds"]+1), 
-                    "label": "reale Kennlinie", 
-                    "color": "blue",
-                    "linestyle": "solid"},
-              "y_ideal": {"curve": np.linspace(0, figure_params["bounds"], figure_params["bounds"]+1), 
+              "y_adu": {"curve": np.zeros(figure_params["bounds"]+1), 
+                        "label": "ADU Kennlinie", 
+                        "color": "blue",
+                        "linestyle": "solid"},
+              "y_ideal": {"curve": np.zeros(figure_params["bounds"]+1),
                           "label": "ideale Kennlinie", 
-                          "color": "red",
+                          "color": "green",
                           "linestyle": "dashed"},
-              "y_offset_error": {"curve": np.linspace(0, figure_params["bounds"], figure_params["bounds"]+1), 
-                                 "label": "Kennlinie mit Offset-Fehler", 
-                                 "color": "green",
-                                 "linestyle": "dashdot"},
-              "y_amplification_error": {"curve": np.linspace(0, figure_params["bounds"], figure_params["bounds"]+1), 
-                                        "label": "Kennlinie mit Verstärkungs-Fehler", 
-                                        "color": "yellowgreen",
-                                        "linestyle": "dashdot"},
-              "y_linearity_error": {"curve": np.linspace(0, figure_params["bounds"], figure_params["bounds"]+1),
-                                    "label": "Kennlinie mit Linearitäts-Fehler", 
-                                    "color": "darkgreen",
-                                    "linestyle": "dashdot"}}
+              "y_delta": {"curve": np.zeros(figure_params["bounds"]+1), 
+                          "label": "Quantisierungsrauschen", 
+                          "color": "red",
+                          "linestyle": "solid"}}
     
     bits_scale_params = {"variable": None,
-                         "from": 0,
-                         "to": 128}
-    resolution_text = "Resolution: "
+                         "from": 2,
+                         "to": 8}
+    resolution_text = None
 
     def __init__(self, title = "Test Window"):
         super().__init__()
@@ -62,7 +54,6 @@ class TKWindow(tk.Tk):
         self.init_axes()
         self.create_widgets()
         self.place_widgets()
-        self.init_widgets()
     
     def init_window(self, title):
         self.title(title)
@@ -71,7 +62,8 @@ class TKWindow(tk.Tk):
         # self.columnconfigure(1, weight=10)
     
     def init_params(self):
-        self.widget_params["adu_bits"]["variable"] = tk.DoubleVar()
+        self.bits_scale_params["variable"] = tk.DoubleVar()
+        self.resolution_text = tk.StringVar()
     
     def init_figure(self):
         self.figure = Figure(figsize=self.figure_params["figsize"], dpi=100)
@@ -80,32 +72,34 @@ class TKWindow(tk.Tk):
     def init_axes(self):
         self.axes1 = self.figure.add_subplot(1, 2, 1)   # Characteristics and ideal curve
         self.axes2 = self.figure.add_subplot(1, 2, 2)   # Delta between characteristic and ideal curve
-        # self.axes3 = self.figure.add_subplot(2, 2, 4)
     
     def create_widgets(self):
         self.bits_scale = tk.Scale(self, 
                                    showvalue=1, 
                                    orient='horizontal', 
-                                   variable=self.widget_params["adu_bits"]["variable"], 
-                                   from_=self.widget_params["adu_bits"]["from"], 
-                                   to=self.widget_params["adu_bits"]["to"])
-        self.resolution = tk.Text(self)#, padx=5, pady=5)
+                                   variable=self.bits_scale_params["variable"], 
+                                   from_=self.bits_scale_params["from"], 
+                                   to=self.bits_scale_params["to"])
+        self.resolution = tk.Label(self, textvariable=self.resolution_text)#, padx=5, pady=5)
 
     def place_widgets(self):
         self.figure_canvas.get_tk_widget().grid(row = 0, column = 0, columnspan = 1)
         
-        self.bits_scale.grid(row = 1, column = 1, sticky = 'ew', pady = 10, padx = 40)
-        self.resolution.grid(row = 2, column = 1, sticky = 'ew', pady = 10, padx = 40)
-    
-    def init_widgets(self):
-        self.resolution.insert(0, self.resolution_text)
+        self.bits_scale.grid(row = 1, column = 0, sticky = 'ew', pady = 10, padx = 40)
+        self.resolution.grid(row = 2, column = 0, sticky = 'ew', pady = 10, padx = 40)
     
     def window_loop(self):
-        self.animation_object = animation.FuncAnimation(self.figure, self.animate, interval = 100)
+        self.animation_object = animation.FuncAnimation(self.figure, self.animate, interval = 200)
         self.mainloop()
     
     def animate(self, i):  # ToDo
-        self.update_graphs()
+        bits = self.bits_scale.get()
+        
+        new_resolution = self.calculate_resolution(bits)
+        
+        self.update_resolution_label(new_resolution)
+        self.update_graphs(bits, new_resolution)
+        
         self.axes1.clear()
         self.axes1.set_title(self.figure_params["title 1"])
         self.axes1.set_xlabel(self.figure_params["xlabel 1"])
@@ -114,13 +108,11 @@ class TKWindow(tk.Tk):
         self.axes1.grid()
         self.axes1.minorticks_on()
         self.axes1.set_xbound(lower = 0, upper = self.figure_params["bounds"])
-        self.axes1.set_ybound(lower = 0, upper = self.figure_params["bounds"])
-        self.plot_axes(self.graphs["y"])
-        self.plot_axes(self.graphs["y_ideal"])
-        self.plot_axes(self.graphs["y_offset_error"]) if self.widget_params['offset_checkbox']["variable"].get() else None
-        self.plot_axes(self.graphs["y_amplification_error"]) if self.widget_params['amplification_checkbox']["variable"].get() else None
-        self.plot_axes(self.graphs["y_linearity_error"]) if self.widget_params['linearity_checkbox']["variable"].get() else None
+        self.axes1.set_ybound(lower = 0, upper = bits**2 - 1)
+        self.plot_axes(self.axes1, self.graphs["y_adu"])
+        self.plot_axes(self.axes1, self.graphs["y_ideal"])
         self.axes1.legend(loc=self.figure_params["legend_position"])
+        self.set_tick_labels(bits)
         
         self.axes2.clear()
         self.axes2.set_title(self.figure_params["title 2"])
@@ -130,27 +122,41 @@ class TKWindow(tk.Tk):
         self.axes2.grid()
         self.axes2.minorticks_on()
         self.axes2.set_xbound(lower = 0, upper = self.figure_params["bounds"])
-        self.axes2.set_ybound(lower = 0, upper = self.figure_params["bounds"])
+        self.axes2.set_ybound(lower = -0.5, upper = 0.5)
+        self.plot_axes(self.axes2, self.graphs["y_delta"])
     
-    def update_graphs(self): # ToDo
-        a = self.widget_params["offset_scale"]["variable"].get() if self.widget_params['offset_checkbox']["variable"].get() else 0
-        b = 1 + self.widget_params["amplification_scale"]["variable"].get()/(100 if self.widget_params["amplification_scale"]["variable"].get() < 0 else 10) \
-        if self.widget_params['amplification_checkbox']["variable"].get() else 1
-        c = self.widget_params["linearity_scale"]["variable"].get() if self.widget_params['linearity_checkbox']["variable"].get() else 0
-        d = self.figure_params["bounds"]/2
-        
+    def calculate_resolution(self, bits):
+        return self. figure_params["bounds"] / (bits**2 -1)
+    
+    def update_resolution_label(self, resolution):
+        self.resolution_text.set("Resolution: {:.2f}".format(resolution))
+    
+    def update_graphs(self, bits, resolution):    
         for x in range(0, self.figure_params["bounds"]+1):
-            self.graphs["y"]["curve"][x] = a + b*x - c*((x/d - 1)**2 - 1)
-            self.graphs["y_offset_error"]["curve"][x] = a + x
-            self.graphs["y_amplification_error"]["curve"][x] = b*x
-            self.graphs["y_linearity_error"]["curve"][x] = x - c*((x/d - 1)**2 - 1)
+            self.graphs["y_adu"]["curve"][x] = (x+resolution/2)//resolution
+            self.graphs["y_ideal"]["curve"][x] = (x*(bits**2 - 1))/self.figure_params["bounds"]
+            self.graphs["y_delta"]["curve"][x] = self.graphs["y_ideal"]["curve"][x] - self.graphs["y_adu"]["curve"][x]
     
-    def plot_axes(self, graph): # ToDo
-        self.axes.plot(self.graphs["x"], 
+    def plot_axes(self, axes, graph):
+        axes.plot(self.graphs["x"], 
                        graph["curve"], 
                        label = graph["label"], 
                        color = graph["color"], 
                        linestyle = graph["linestyle"])
+    
+    def set_tick_labels(self, bits):
+        ticks = [] 
+        labels = []
+        
+        for i in range(0, bits**2):
+            if bits < 6:
+                ticks.append(i)
+                labels.append(f"{i:b}".zfill(bits))
+            elif i%2:
+                ticks.append(i)
+                labels.append(f"{i:b}".zfill(bits))
+        
+        self.axes1.set_yticks(ticks, labels)
 
 if __name__ == "__main__":
     exercise_03 = TKWindow(window_title)
